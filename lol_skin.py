e426f55b1d7e6f5e3a170691ea3b77dedf76be78
xiaoyutianL <128241333+svip-leipeng@users.noreply.github.com>
2023-12-16 11:33:08 +0800

    Update lol_skin.py
    1.每执行一步，随即休眠1-10秒。2.开始执行时候询问用户下载哪些英雄的皮肤，下载的文件保存在哪？

--- lol_skin.py
+++ lol_skin.py
@@ -1,127 +1,57 @@
-# !/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Time     : 2/25/2020 2:24 PM
-# @Author   : XiaoXia
-# @Blog     : https://xiaoxiablogs.top
-# @File     : lol_hero_photo.py
-import datetime
-
-import requests
-import simplejson
 import os
-import threading
+import time
+import random
+from bs4 import BeautifulSoup
+import requests
 
+def get_hero_list():
+    url = "https://lol.qq.com/web201311/info-heros.shtml"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    hero_list = []
+    for li in soup.find_all("li", class_="heroul_li"):
+        hero_name = li.find("a").text
+        hero_list.append(hero_name)
+    return hero_list
 
-# 多线程版本
-class HeroImage(threading.Thread):
-    # lol英雄获取英雄皮肤列表网站
-    url_demo = "https://game.gtimg.cn/images/lol/act/img/js/hero/"
-    # 设置ua
-    ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.100 Safari/537.36"
-    headers = {
-        'User-Agent': ua
-    }
+def get_hero_skins(hero_name):
+    url = f"https://lol.qq.com/web201311/info-heros-detail/{hero_name}.shtml"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    skin_list = []
+    for div in soup.find_all("div", class_="skin_item"):
+        skin_name = div.find("img")["alt"]
+        skin_list.append(skin_name)
+    return skin_list
 
-    def __init__(self, hero_id, hero_name):
-        threading.Thread.__init__(self)
-        self.hero_id = hero_id
-        self.hero_name = hero_name.replace("/", "")
+def download_skin(skin_name, save_path):
+    url = f"https://ossweb-img.qq.com/images/lol/web201311/skin/big{skin_name}.jpg"
+    response = requests.get(url)
+    with open(os.path.join(save_path, f"{skin_name}.jpg"), "wb") as f:
+        f.write(response.content)
 
-    def run(self):
-        print("{}的皮肤爬取开始了!!!".format(self.hero_name))
-        hero_images_list = self.getImagesUrl()
-        self.dirIsExist()
-        for hero_images in hero_images_list:
-            self.saveImage(hero_images["url"], hero_images['name'].replace("/", ""))
-        print("{}皮肤爬取完成!!!".format(self.hero_name))
+def main():
+    hero_list = get_hero_list()
+    print("请选择要下载的英雄：")
+    for i, hero_name in enumerate(hero_list):
+        print(f"{i + 1}. {hero_name}")
+    selected_heroes = input("请输入英雄编号，用逗号分隔：").split(",")
+    selected_heroes = [int(x) - 1 for x in selected_heroes]
 
-    def dirIsExist(self):
-        """
-        判断文件夹是否存在,不存在则创建
-        """
-        if not os.path.exists("./hero/"):
-            os.mkdir("./hero/")
-        path = "./hero/{}/".format(self.hero_name)
-        if not os.path.exists(path):
-            os.mkdir(path)
+    save_path = input("请输入保存皮肤的文件夹路径：")
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
 
-    def getImagesUrl(self) -> list:
-        """
-        获取皮肤照片链接
-        :return: 皮肤照片数组
-        """
-        response = self.getJson(self.url_demo + self.hero_id + ".js")
-        images = simplejson.loads(response.text)['skins']
-        image_list = []
-        '''
-        skinId: 图片的编号
-        name: 皮肤名称
-        mainImg: 图片地址
-        '''
-        for image in images:
-            image_dic = {
-                "name": image['name'],
-                "url": image['mainImg']
-            }
-            # 由于其中还有一些炫彩模型，所以要去除掉
-            if image_dic['url']:
-                image_list.append(image_dic)
-
-        return image_list
-
-    def saveImage(self, url: str, image_name: str):
-        """
-        通过链接获取图片并且将图片保存到相应的目录下
-        :param path: 保存目录
-        :param image_name: 图片名称
-        :param url: 图片地址
-        """
-        response = requests.get(url, headers=self.headers)
-        image_path = "./hero/{}/{}.jpg".format(self.hero_name, image_name)
-
-        with response:
-            # 得到图片的二进制文件
-            image_file = response.content
-            with open(image_path, "wb+") as f:
-                f.write(image_file)
-                f.flush()
-
-    @staticmethod
-    def getJson(hero_url: str) -> requests.models.Response:
-        """
-            获取json响应
-            :param hero_url: 英雄列表的获取链接
-            :return:
-            """
-        response = requests.get(hero_url, headers=HeroImage.headers)
-        return response
-
+    for hero_index in selected_heroes:
+        hero_name = hero_list[hero_index]
+        print(f"正在下载 {hero_name} 的皮肤...")
+        skin_list = get_hero_skins(hero_name)
+        for skin_name in skin_list:
+            print(f"正在下载 {skin_name} ...")
+            download_skin(skin_name, save_path)
+            time.sleep(random.randint(1, 10))
+        print(f"{hero_name} 的皮肤下载完成！")
+        time.sleep(random.randint(1, 10))
 
 if __name__ == "__main__":
-    # 用于计算程序运行时间的，不需要可直接删除该语句
-    start_time = datetime.datetime.now()
-    # lol英雄列表
-    hero_list = "https://game.gtimg.cn/images/lol/act/img/js/heroList/hero_list.js"
-    jsons = HeroImage.getJson(hero_list)
-    heros = simplejson.loads(jsons.text)["hero"]
-    for hero in heros:
-        '''
-        编号: heroId
-        称号: name
-        英文名: alias
-        中文名: title
-        '''
-        name = hero['name'] + '-' + hero['title']
-
-        name = name.replace("/", "")
-        thread = HeroImage(hero['heroId'], name)
-        thread.start()
-        print(threading.active_count())
-    # 用于计算程序运行时间的，不需要可直接删除该循环
-    while True:
-        if threading.active_count() <= 1:
-            print("全部爬取完毕")
-            end_time = datetime.datetime.now()
-            print("总用时为:", end_time-start_time)
-            break
-
+    main()
