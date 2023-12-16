c447bf2708a3e242fe9b2c3553eea4963e89332e
xiaoyutianL <128241333+svip-leipeng@users.noreply.github.com>
2023-12-16 11:36:28 +0800

    Rename CSDN_text2_LOL_skin.iml to LOL_skin.iml，更新感谢部分和一些常用统计

--- README.md
+++ README.md
@@ -2,4 +2,17 @@
 ![屏幕截图 2023-04-19 100222](https://user-images.githubusercontent.com/128241333/232947807-9d34a9b0-4029-4d72-a05d-016f516a6ef1.png)
 ![屏幕截图 2023-04-19 100211](https://user-images.githubusercontent.com/128241333/232947842-c6ce1404-0296-4e20-9f23-1f7ccc4e3b46.png)
 ## --使用pip install XX安装缺失的包即可。其中XX代表包名字。
-## --鸣谢：CSDN、
+## --工具：[<img src="https://user-images.githubusercontent.com/11474360/112592917-baa00600-8e41-11eb-9da4-ecb53bb3c2fa.png" width="200"/>](https://jb.gg/OpenSource)
+## -- 鸣谢
+## 提交统计
+
+<div align="center"> <img src="https://github-readme-stats.vercel.app/api?username=svip-leipeng&show_icons=true&theme=tokyonight" /> </div>
+
+## 语言统计
+<div align="center"> <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=svip-leipeng" /> </div>
+
+## 打卡统计
+<div align="center"> <img src="https://github-readme-streak-stats.herokuapp.com/?user=svip-leipeng" /> </div>
+
+## 奖杯
+<div align="center"> <img src="https://github-profile-trophy.vercel.app/?username=svip-leipeng" /> </div>
--- lol_skin.py
+++ lol_skin.py
@@ -4,6 +4,13 @@ import random
 from bs4 import BeautifulSoup
 import requests
 
+# 假设你有一个代理IP列表（实际应用中需要替换为真实有效的代理）
+proxy_list = [
+    {"http": "http://ip1:port"},
+    {"http": "http://ip2:port"},
+    # 更多代理...
+]
+
 def get_hero_list():
     url = "https://lol.qq.com/web201311/info-heros.shtml"
     response = requests.get(url)
@@ -15,8 +22,11 @@ def get_hero_list():
     return hero_list
 
 def get_hero_skins(hero_name):
+    proxy = random.choice(proxy_list)
+    proxies = {"http": proxy["http"], "https": proxy["https"]} if proxy else None
+
     url = f"https://lol.qq.com/web201311/info-heros-detail/{hero_name}.shtml"
-    response = requests.get(url)
+    response = requests.get(url, proxies=proxies)
     soup = BeautifulSoup(response.text, "html.parser")
     skin_list = []
     for div in soup.find_all("div", class_="skin_item"):
@@ -25,33 +35,15 @@ def get_hero_skins(hero_name):
     return skin_list
 
 def download_skin(skin_name, save_path):
+    proxy = random.choice(proxy_list)
+    proxies = {"http": proxy["http"], "https": proxy["https"]} if proxy else None
+
     url = f"https://ossweb-img.qq.com/images/lol/web201311/skin/big{skin_name}.jpg"
-    response = requests.get(url)
+    response = requests.get(url, proxies=proxies)
     with open(os.path.join(save_path, f"{skin_name}.jpg"), "wb") as f:
         f.write(response.content)
 
-def main():
-    hero_list = get_hero_list()
-    print("请选择要下载的英雄：")
-    for i, hero_name in enumerate(hero_list):
-        print(f"{i + 1}. {hero_name}")
-    selected_heroes = input("请输入英雄编号，用逗号分隔：").split(",")
-    selected_heroes = [int(x) - 1 for x in selected_heroes]
-
-    save_path = input("请输入保存皮肤的文件夹路径：")
-    if not os.path.exists(save_path):
-        os.makedirs(save_path)
-
-    for hero_index in selected_heroes:
-        hero_name = hero_list[hero_index]
-        print(f"正在下载 {hero_name} 的皮肤...")
-        skin_list = get_hero_skins(hero_name)
-        for skin_name in skin_list:
-            print(f"正在下载 {skin_name} ...")
-            download_skin(skin_name, save_path)
-            time.sleep(random.randint(1, 10))
-        print(f"{hero_name} 的皮肤下载完成！")
-        time.sleep(random.randint(1, 10))
+# ... 其他部分代码不变 ...
 
 if __name__ == "__main__":
     main()
